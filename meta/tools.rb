795318ae939e932cfecf970533c43262a2522262
textarcana <noah@onemorebug.com>
2009-07-02 08:04:50 -0400

    learning ways to identify assertions as a test is running

--- meta/tools.rb
+++ meta/tools.rb
@@ -6,18 +6,68 @@
 
 require 'test/unit'
 
-notline24 = $. == 24
+class MetaTools
 
-# puts $.
+  def basics
 
-# puts $0
+    # $. is the last line executed by the interpreter, so it starts
+    # counting from the first library loaded and is therefore not useful
+    # for giving helpful feedback about an application under test
 
-tools_rb = $0 == "tools.rb"
+    notline17 = $. == 17
 
-line34 =  __LINE__ == 34
+    # puts $.
 
-# puts __LINE__
+    # puts $0
 
-#puts line24 && 
+    tools_rb = $0 == "tools.rb"
 
-puts tools_rb && line34 && ( ! notline24) 
+    line25 =  __LINE__ == 25
+
+    # puts __LINE__
+
+    #puts line24 && 
+
+    tools_rb && line25 && ( ! notline17) 
+
+  end
+
+  def initialize
+
+  end
+
+end
+
+class TestMetaTools < Test::Unit::TestCase
+
+  def test_sanity
+
+    m = MetaTools.new
+
+    assert m.basics == true
+
+  end
+
+  def test_name_of_test
+
+    #    puts self.instance_variables.sort
+
+    warn_with_line.call
+
+    assert_equal self.name, "test_name_of_test(TestMetaTools)"
+
+  end
+
+  def warn_with_line
+
+    f = lambda{
+    
+      warn "#{__FILE__}:#{__LINE__}: there is an error in #{self.name}"
+      
+    }
+
+  end
+
+end
+
+
